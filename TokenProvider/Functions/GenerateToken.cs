7cb7fa8335970b73764d29de2b20cb985b8b38cd
Markus Lokander <Markuslokander97@gmail.com>
2024-05-21 16:09:02 +0200

    Add project files.

---
+++ TokenProvider/Functions/GenerateToken.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TokenProvider.Functions;
+
+public class GenerateToken
+{
+    private readonly ILogger<GenerateToken> _logger;
+
+    public GenerateToken (ILogger<GenerateToken> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GenerateToken")]
+
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+        return new OkObjectResult("");
+    }
+}
