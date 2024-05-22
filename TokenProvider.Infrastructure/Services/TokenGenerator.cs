20fa0d9937386841adb40e66f0b674bc127ce972
Markus Lokander <Markuslokander97@gmail.com>
2024-05-22 14:52:49 +0200

    generation of token done, cookie handler next

---
+++ TokenProvider.Infrastructure/Services/TokenGenerator.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using TokenProvider.Infrastructure.Models;
+
+namespace TokenProvider.Infrastructure.Services;
+
+public class TokenGenerator(IRefreshTokenService refreshTokenService)
+{
+    private readonly IRefreshTokenService _refreshTokenService = refreshTokenService;
+
+
+    public async Task<RefreshTokenResult> GenerateRefreshTokenAsync( string userId, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(userId))
+                return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.BadRequest, Error = "Invalid body request. No userid was found" };
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId)
+            };
+
+            var token = GenerateJwtToken(new ClaimsIdentity(claims), DateTime.Now.AddMinutes(5));
+            if (token == null)
+                return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = "An error lol" };
+
+
+        }catch (Exception ex) 
+        {
+            return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = ex.Message };
+        }
+    }
+
+
+    public static string GenerateJwtToken (ClaimsIdentity claims, DateTime expires)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = claims,
+            Expires = expires,
+            Issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER"),
+            Audience = Environment.GetEnvironmentVariable("TOKEN_AUDIENCE"),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("TOKEN_SECRETKEY")!)), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
--- TokenProvider/Functions/GenerateToken.cs
+++ TokenProvider/Functions/GenerateToken.cs
@@ -3,25 +3,46 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using TokenProvider.Infrastructure.Models;
+using TokenProvider.Infrastructure.Services;
 
 namespace TokenProvider.Functions;
 
 public class GenerateToken
 {
     private readonly ILogger<GenerateToken> _logger;
+    private readonly IRefreshTokenService _refreshTokenService;
 
-    public GenerateToken (ILogger<GenerateToken> logger)
+    public GenerateToken(ILogger<GenerateToken> logger, IRefreshTokenService refreshTokenService)
     {
         _logger = logger;
+        _refreshTokenService = refreshTokenService;
     }
 
     [Function("GenerateToken")]
 
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "token/generate")] HttpRequest req, [FromBody] TokenRequest tokenRequest)
+    public async IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "token/generate")] HttpRequest req, [FromBody] TokenRequest tokenRequest)
     {
 
         if (tokenRequest.UserId == null || tokenRequest.Email == null)
             return new BadRequestObjectResult(new { Error = "Please provide a valid email" });
+
+
+        try
+        {
+            RefreshTokenResult refreshTokenResult = null!;
+            using var ctsTimeOut = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+            using var cts = CancellationTokenSource.CreateLinkedTokenSource(ctsTimeOut.Token, req.HttpContext.RequestAborted);
+
+            req.HttpContext.Request.Cookies.TryGetValue("refreshToken", out var refreshToken);
+            if (!string.IsNullOrEmpty(refreshToken))
+                refreshTokenResult = await _refreshTokenService.GetRefreshTokenAsync(refreshToken, cts.Token);
+
+            if ( refreshTokenResult == null ||refreshTokenResult.ExpiryDate < DateTime.Now.AddDays(1))
+                refreshTokenResult = await _tokenGenerator.GenerateRefreshTokenAsync(tokenRequest.UserId, cts.Token);
+        }catch 
+        {
+
+        }
         
 
         
