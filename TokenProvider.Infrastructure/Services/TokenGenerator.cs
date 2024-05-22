c8476d3d5190b665b9e0948488e703c2c96ca6ae
Markus Lokander <Markuslokander97@gmail.com>
2024-05-22 16:49:36 +0200

    accesstoken created

--- TokenProvider.Infrastructure/Services/TokenGenerator.cs
+++ TokenProvider.Infrastructure/Services/TokenGenerator.cs
@@ -8,12 +8,18 @@ using TokenProvider.Infrastructure.Models;
 
 namespace TokenProvider.Infrastructure.Services;
 
-public class TokenGenerator(IRefreshTokenService refreshTokenService)
+public interface ITokenGenerator
+{
+    AccessTokenResult GenerateAccessToken(TokenRequest tokenRequest, string? refreshToken);
+    Task<RefreshTokenResult> GenerateRefreshTokenAsync(string userId, CancellationToken cancellationToken);
+}
+
+public class TokenGenerator(IRefreshTokenService refreshTokenService) : ITokenGenerator
 {
     private readonly IRefreshTokenService _refreshTokenService = refreshTokenService;
 
-
-    public async Task<RefreshTokenResult> GenerateRefreshTokenAsync( string userId, CancellationToken cancellationToken)
+    #region GenerateRefreshTokenAsync
+    public async Task<RefreshTokenResult> GenerateRefreshTokenAsync(string userId, CancellationToken cancellationToken)
     {
         try
         {
@@ -27,29 +33,78 @@ public class TokenGenerator(IRefreshTokenService refreshTokenService)
 
             var token = GenerateJwtToken(new ClaimsIdentity(claims), DateTime.Now.AddMinutes(5));
             if (token == null)
-                return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = "An error lol" };
+                return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = "An error token lol" };
+
+            var cookieOptions = CookieGenerator.GenerateCookie(DateTimeOffset.Now.AddDays(7));
+            if (cookieOptions == null)
+                return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = "An error cookie lol" };
+
+            var result = await _refreshTokenService.SaveRefreshTokenAsync(token, userId, cancellationToken);
+            if (!result)
+                return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = "An error saving stuff lol" };
 
 
-        }catch (Exception ex) 
+            return new RefreshTokenResult
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Token = token,
+                CookieOptions = cookieOptions
+            };
+
+
+        }
+        catch (Exception ex)
         {
             return new RefreshTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = ex.Message };
         }
     }
 
+    #endregion
+    public AccessTokenResult GenerateAccessToken(TokenRequest tokenRequest, string? refreshToken)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(tokenRequest.UserId) || string.IsNullOrEmpty(tokenRequest.Email))
+                return new AccessTokenResult { StatusCode = (int)HttpStatusCode.BadRequest, Error = "missing userid or email" };
 
-    public static string GenerateJwtToken (ClaimsIdentity claims, DateTime expires)
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, tokenRequest.UserId),
+                new Claim(ClaimTypes.Name, tokenRequest.Email),
+                new Claim(ClaimTypes.Email, tokenRequest.Email),
+            };
+
+            if (!string.IsNullOrEmpty(refreshToken))
+                claims = [.. claims, new Claim("refreshToken", refreshToken)];
+
+            var token = GenerateJwtToken(new ClaimsIdentity(claims), DateTime.Now.AddMinutes(5));
+
+            if (token == null)
+                return new AccessTokenResult { StatusCode = (int)HttpStatusCode.BadRequest, Error = "accesstoken could not be generated" };
+
+            return new AccessTokenResult { StatusCode = (int)(int)HttpStatusCode.OK, Token = token };
+        }
+        catch (Exception ex)
+        {
+            return new AccessTokenResult { StatusCode = (int)HttpStatusCode.InternalServerError, Error = ex.Message };
+        }
+    }
+
+
+    public static string GenerateJwtToken(ClaimsIdentity claims, DateTime expires)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = claims,
-            Expires = expires,
-            Issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER"),
-            Audience = Environment.GetEnvironmentVariable("TOKEN_AUDIENCE"),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("TOKEN_SECRETKEY")!)), SecurityAlgorithms.HmacSha256Signature)
-        };
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = claims,
+                    Expires = expires,
+                    Issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER"),
+                    Audience = Environment.GetEnvironmentVariable("TOKEN_AUDIENCE"),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("TOKEN_SECRETKEY")!)), SecurityAlgorithms.HmacSha256Signature)
+                };
 
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
     }
+ 
 }
--- TokenProvider/Functions/GenerateToken.cs
+++ TokenProvider/Functions/GenerateToken.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
@@ -7,20 +8,15 @@ using TokenProvider.Infrastructure.Services;
 
 namespace TokenProvider.Functions;
 
-public class GenerateToken
+public class GenerateToken(ILogger<GenerateToken> logger, IRefreshTokenService refreshTokenService, ITokenGenerator tokenGenerator)
 {
-    private readonly ILogger<GenerateToken> _logger;
-    private readonly IRefreshTokenService _refreshTokenService;
-
-    public GenerateToken(ILogger<GenerateToken> logger, IRefreshTokenService refreshTokenService)
-    {
-        _logger = logger;
-        _refreshTokenService = refreshTokenService;
-    }
+    private readonly ILogger<GenerateToken> _logger = logger;
+    private readonly IRefreshTokenService _refreshTokenService = refreshTokenService;
+    private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
 
     [Function("GenerateToken")]
 
-    public async IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "token/generate")] HttpRequest req, [FromBody] TokenRequest tokenRequest)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "token/generate")] HttpRequest req, [FromBody] TokenRequest tokenRequest)
     {
 
         if (tokenRequest.UserId == null || tokenRequest.Email == null)
@@ -30,6 +26,7 @@ public class GenerateToken
         try
         {
             RefreshTokenResult refreshTokenResult = null!;
+            AccessTokenResult accessTokenResult = null!;
             using var ctsTimeOut = new CancellationTokenSource(TimeSpan.FromSeconds(3));
             using var cts = CancellationTokenSource.CreateLinkedTokenSource(ctsTimeOut.Token, req.HttpContext.RequestAborted);
 
@@ -39,13 +36,19 @@ public class GenerateToken
 
             if ( refreshTokenResult == null ||refreshTokenResult.ExpiryDate < DateTime.Now.AddDays(1))
                 refreshTokenResult = await _tokenGenerator.GenerateRefreshTokenAsync(tokenRequest.UserId, cts.Token);
+
+            accessTokenResult = _tokenGenerator.GenerateAccessToken(tokenRequest, refreshTokenResult.Token);
+            if (accessTokenResult != null && accessTokenResult.Token != null && refreshTokenResult.Token != null && refreshTokenResult.CookieOptions != null)
+            {
+                req.HttpContext.Response.Cookies.Append("refreshToken", refreshTokenResult.Token, refreshTokenResult.CookieOptions);
+                return new OkObjectResult(new { AccessToken = accessTokenResult.Token, RefreshToken = refreshTokenResult.Token });
+            }
+                return new ObjectResult(new { Error = "error generating tokens" }) { StatusCode = 500 };
+
         }catch 
         {
 
         }
-        
-
-        
-        return new OkObjectResult("");
+         return new OkObjectResult("");
     }
 }
